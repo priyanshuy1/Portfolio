cbbd77696f99a2c3c6a3d910ca5213faa70239dd
priyanshuy1 <yadavrockey82@gmail.com>
2026-04-15 23:57:12 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,12 @@
-console.log("Portfolio Loaded Successfully 🚀");
+const text = "Hi, I'm Priyanshu Yadav";
+let i = 0;
+
+function typingEffect() {
+  if (i < text.length) {
+    document.getElementById("typing").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typingEffect, 80);
+  }
+}
+
+typingEffect();
