b824ef23bee61ff34d38fff2fe86b496cea67358
priyanshuy1 <yadavrockey82@gmail.com>
2026-04-16 11:00:31 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -2,11 +2,13 @@ const text = "Hi, I'm Priyanshu Yadav";
 let i = 0;
 
 function typingEffect() {
+  const typing = document.getElementById("typing");
+
   if (i < text.length) {
-    document.getElementById("typing").innerHTML += text.charAt(i);
+    typing.textContent += text.charAt(i);
     i++;
-    setTimeout(typingEffect, 80);
+    setTimeout(typingEffect, 70);
   }
 }
 
-typingEffect();
+window.onload = typingEffect;
